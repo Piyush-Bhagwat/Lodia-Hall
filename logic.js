d58dc0f9d8f86ff1cadf9893d2a247c9d29ff44f
Piyush-Bhagwat <piyushbhagwat5@gmail.com>
2022-11-30 00:09:07 +0530

    optimised For Phone

--- logic.js
+++ logic.js
@@ -50,14 +50,4 @@ hiddentELements.forEach((el) => observer.observe(el));
 const hiddenTitle = document.querySelectorAll('.titleHidden');
 hiddenTitle.forEach((el) => titleObserver.observe(el));
 
-// var navbar = document.getElementById("navLinks");
-// var sticky = navbar.offsetTop;
-
-// window.onscroll = function () {
-//     if (window.pageYOffset >= sticky) {
-//         navbar.classList.add("sticky")
-//       } else {
-//         navbar.classList.remove("sticky");
-//       }
-// };
 
